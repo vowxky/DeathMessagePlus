789547cc7f74767cba9a7b792156dbfddbfcb56b
Vowxky <114261590+vowxky@users.noreply.github.com>
2023-07-25 13:51:59 -0500

    Especificacion nueva agregada

--- src/main/java/vowxky/deathmessageplus/config/ConfigSetup.java
+++ src/main/java/vowxky/deathmessageplus/config/ConfigSetup.java
@@ -11,7 +11,7 @@ public class ConfigSetup {
         displayscreen = serverConfigBuilder.comment("Para habilitar el mensaje en pantalla o en el chat || El default es true significara que mostrara el mensaje por pantalla")
                 .define("DisplayScreen", true);
 
-        chatColor = serverConfigBuilder.comment("Puedes agregar el color que mas gustes al mensaje de muerte , dejare una lista de variantes que por cierto deben estar escritas en mayusculas total para evitar errores" +
+        chatColor = serverConfigBuilder.comment("Puedes agregar el color que mas gustes al mensaje de muerte , tener en cuenta que del numero 6 en adelante no tiene color asi que para evitar errores tratemos de solo elegir entre las opciones que hay <3" +
                         "\n" +
                         "1 --> ROJO\n" +
                         "2 --> AZUL\n" +
